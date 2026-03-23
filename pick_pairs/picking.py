68ecd986c28f5fabaca7de3b339a8687ed89d754
Finn Harbeke <code@finnharbeke.ch>
2026-03-23 15:35:23 +0100

    path entropy

--- pick_pairs/picking.py
+++ pick_pairs/picking.py
@@ -6,9 +6,15 @@ import itertools
 import plotly.express as px
 import plotly
 
+def entropy_mode_freq(col):
+    freq = col.value_counts(normalize=True)
+    return -(freq * np.log(freq)).sum(), freq.iloc[0]
+
 if __name__ == "__main__":
     sns.set()
     
+    # the dist(u,v) random variable, its mean and std
+    #################################################
     dists = np.load('sh_dists.npy')
 
     mean = dists.mean(axis=0)
@@ -25,5 +31,21 @@ if __name__ == "__main__":
     sns.histplot(dist_uv, x='std')
     plt.savefig('sh_hist.png')
     fig = px.scatter(dist_uv, x='mean', y='std', color='u', hover_data=['u', 'v'])
+    fig.update_traces(marker=dict(size=10))
     plotly.offline.plot(fig, filename='sh_dist_scatter.html')
     print(dist_uv.head())
+    
+    # the SP(u, v) random variable, its entropy and the mode's frequency
+    ###################################################################### 
+    
+    paths = pd.read_csv('sh_paths.csv', dtype=str, index_col=0)
+    paths.fillna('', inplace=True)
+    print(paths.head())
+    path_stats = pd.DataFrame(columns=['entropy', 'mode_freq', 'u', 'v'])
+    for col_name in paths.columns:
+        u, v = [int(x) for x in col_name.split('-')]
+        path_stats.loc[col_name] = (*entropy_mode_freq(paths[col_name]), u, v)
+
+    fig = px.scatter(path_stats, x='entropy', y='mode_freq', color='u', hover_data=['u', 'v'])
+    fig.update_traces(marker=dict(size=10))
+    plotly.offline.plot(fig, filename='sh_path_scatter.html')
